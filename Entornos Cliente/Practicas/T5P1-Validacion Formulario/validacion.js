8991f53ebd4d8c4310ff074a001df9b165a48f38
amolina19 <alejandro.molina.dam@gmail.com>
2020-12-17 09:52:00 +0100

    T5P1 sin recoger datos en js

---
+++ Entornos Cliente/Practicas/T5P1-Validacion Formulario/validacion.js
@@ -0,0 +1,30 @@
+    
+var nombre = document.getElementById("nombre");
+var apellidos = document.getElementById("apellidos");
+var edad = document.getElementById("edad");
+var nif = document.getElementById("nif");
+var email = document.getElementById("email");
+var provincia = document.getElementById("provincia");
+var fechanac = document.getElementById("fecha");
+var telefono = document.getElementById("telefono");
+var visita = document.getElementById("horavisita");
+
+
+var varName = "test";
+//nombre = nombre.value;
+//apellidos = apellidos.value;
+//edad = edad.value;
+//nif = nif.value;
+//email = email.value;
+//provincia = provincia.value;
+//fechanac = fechanac.value;
+//telefono = telefono.value;
+//visita = visita.value;
+//alert(nombre+" "+apellidos+" "+edad+" "+nif+" "+email+" "+provincia+" "+fechanac+" "+telefono+" "+visita);
+    
+
+
+    
+    
+
+
