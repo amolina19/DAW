af913557c82c5ff05a8115aab8d77f13424b04a7
amolina19 <alejandro.molina.dam@gmail.com>
2021-02-03 21:42:17 +0100

    Examen 4 Febrero (Esperemos)

---
+++ Entornos Cliente/Examen Tema 5,6/app.js
@@ -0,0 +1,59 @@
+
+var nombre = null;
+var password = null;
+var buttonSend = null;
+var languange = 0;
+
+window.onload = function(){
+
+    nombre = document.getElementById("nombre");
+    password = document.getElementById("password");
+    buttonSend = document.getElementById("enviar");
+    
+    buttonSend.onclick = function (){
+        if(isAnAdmin()){
+            var buttonAdmin = document.createElement("input");
+            buttonAdmin.value = "Idioma";
+            buttonAdmin.id = "adminbtn";
+            buttonAdmin.setAttribute("type","submit");
+            buttonAdmin.onclick = function(){
+                changeLanguge();
+            }
+            document.body.appendChild(buttonAdmin);
+        }
+    }
+
+    document.getElementById("form").onsubmit = function(event) {
+        event.preventDefault();
+        return false;
+    }
+}
+
+function isAnAdmin(){
+
+    if(nombre.value.toLocaleLowerCase() == "admin" && password.value == 'todopoderoso'){
+        return true;
+    }
+    return false;
+}
+
+function changeLanguge(){
+    if(languange == 0){
+        var elementsp = document.getElementsByTagName("p");
+        elementsp[0].innerHTML = "Name";
+        elementsp[1].innerHTML = "Password";
+        var elementsBtn = document.getElementsByTagName("input");
+        elementsBtn[2].value = "Send";
+        elementsBtn[3].value = "Language"; 
+        languange = 1;
+    }else if(languange == 1){
+        var elementsp = document.getElementsByTagName("p");
+        elementsp[0].innerHTML = "Nombre";
+        elementsp[1].innerHTML = "Contraseña";
+        var elementsBtn = document.getElementsByTagName("input");
+        elementsBtn[2].value = "Enviar";
+        elementsBtn[3].value = "Idioma"; 
+        languange = 0;
+    }
+    
+}
