0bfcc69e82b16738b59037bb54eb99df033c28eb
amolina19 <alejandro.molina.dam@gmail.com>
2021-02-04 13:33:27 +0100

    Inmobiliaria texto arreglado.

--- Entornos Servidor/Tema 3/Ejercicios/Inmobiliaria/inmobiliaria.sql
+++ Entornos Servidor/Tema 3/Ejercicios/Inmobiliaria/inmobiliaria.sql
@@ -26,18 +26,18 @@ CREATE TABLE noticias (
   imagen varchar(100) default NULL,
   PRIMARY KEY (id)
 ) 
-
+/*
 --TYPE=MyISAM COMMENT='Noticias de la inmobiliaria Lindavista';
 
-/*
+
 #
 # Volcar la base de datos para la tabla `noticias`
 #
 */
 
-INSERT INTO noticias VALUES (1, 'Nueva promoción en Nervion', '145 viviendas de lujo en urbanizacion ajardinada situadas en un entorno privilegiado', 'promociones', '2004-02-04', NULL);
+INSERT INTO noticias VALUES (1, 'Nueva promocion en Nervion', '145 viviendas de lujo en urbanizacion ajardinada situadas en un entorno privilegiado', 'promociones', '2004-02-04', NULL);
 INSERT INTO noticias VALUES (2, 'ultimas viviendas junto al rio', 'Apartamentos de 1 y 2 dormitorios, con fantasticas vistas. Excelentes condiciones de financiacion.', 'ofertas', '2004-02-05', NULL);
 INSERT INTO noticias VALUES (3, 'Apartamentos en el Puerto de Sta Maria', 'En la playa de Valdelagrana, en primera linea de playa. Pisos reformados y completamente amueblados.', 'costas', '2004-02-06', 'apartamento8.jpg');
 INSERT INTO noticias VALUES (4, 'Casa reformada en el barrio de la Juderia', 'Dos plantas y Atico, 5 habitaciones, patio interior, amplio garaje. Situada en una calle tranquila y a un paso del centro historico.', 'promociones', '2004-02-07', NULL);
-INSERT INTO noticias VALUES (5, 'Promocion en Costa Ballena', 'Con vistas al campo de golf, magníficas calidades, entorno ajardinado con piscina y servicio de vigilancia.', 'costas', '2004-02-09', 'apartamento9.jpg');
+INSERT INTO noticias VALUES (5, 'Promocion en Costa Ballena', 'Con vistas al campo de golf, magnificas calidades, entorno ajardinado con piscina y servicio de vigilancia.', 'costas', '2004-02-09', 'apartamento9.jpg');
     
