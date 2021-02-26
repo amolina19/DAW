84f2e997f3ca2ef568c851b8d7c8828e8b767c95
amolina19 <alejandro.molina.dam@gmail.com>
2021-02-26 13:36:50 +0100

    Unfinished

--- Entornos Cliente/Tema 8/Bases de datos/Ejercicio/app.js
+++ Entornos Cliente/Tema 8/Bases de datos/Ejercicio/app.js
@@ -8,6 +8,10 @@ var limpiarTodo;
 var guardia;
 var insertarGuard;
 
+var arrayGuardias = new Map();
+var arrayProfesores = new Map();
+var nguardias = new Map();
+
 window.onload = function (){
 
     indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
@@ -27,7 +31,9 @@ window.onload = function (){
     limpiarTodo.onclick = function(){
         limpiar();
     }
+    nGuardiasProfesores();
     updateTables();
+    
 
 }
 
@@ -72,15 +78,21 @@ function insertar(database){
 }
 
 function insertarGuardia(database){
-    var idGuardia = document.getElementById("idguardia").value;
-    var idProfesor = document.getElementById("idprofesorguardia").value;
+    var idGuardia = Number(document.getElementById("idguardia").value);
+    //var idProfesor = document.getElementById("idprofesorguardia").value;
     var ausente = document.getElementById("ausente").value;
+
+    if(ausente == "true"){
+        ausente = true;
+    }else{
+        ausente = false
+    }
     var fecha = document.getElementById("fechaguardia").value;
-    var hora = document.getElementById("horaguardia").value;
+    var hora = Number(document.getElementById("horaguardia").value);
     
     var item = {
         id_guardia: idGuardia,
-        id_profesor: idProfesor,
+        id_profesor: undefined,
         ausente: ausente,
         fecha: fecha,
         hora: hora
@@ -148,6 +160,43 @@ function openDB(){
 
 }
 
+function nGuardiasProfesores(){
+    var database = indexedDB.open("Guardia", 1);
+        database.onsuccess = function(event) {
+        var db = event.target.result;
+        var data = db.transaction(["Guardias"], "readonly");
+        var objectStorage = data.objectStore("Guardias");
+        var elements = [];
+
+        objectStorage.openCursor().onsuccess = function (e) {
+                        
+            var result = e.target.result;
+            if (result === null) {
+                return;
+            }
+            //
+            
+            //alert(result);
+            elements.push(result.value);
+            result.continue();
+            
+        };
+        data.oncomplete = function() {
+            
+            for (var key in elements) {
+
+                guardia = {
+                    id_guardia: elements[key].id_guardia,
+                    id_profesor: elements[key].id_profesor,
+                };
+                nguardias.delete(Number(guardia.id_profesor));
+                nguardias.set(Number(guardia.id_profesor),Number(nguardias.get(guardia.id_profesor)+1));
+            }
+            console.log(nguardias);
+        };
+    }
+}
+
 function generateGuardiaTable(idProfesor){
     //alert(idProfesor);
     var database = indexedDB.open("Guardia", 1);
@@ -172,27 +221,38 @@ function generateGuardiaTable(idProfesor){
         };
 
         data.oncomplete = function() {
-                        
+            let nguardiashechas = 0;
             var outerHTML = "";
             outerHTML += "<table>";
             outerHTML += "<th>Ausente</th><th>Fecha</th><th>Hora</th>";
             outerHTML += "<tr>";
+            
             for (var key in elements) {
 
-                if(elements[key].id_profesor == idProfesor){
-                    if(elements[key].ausente == true){
+                guardia = {
+                    id_guardia: elements[key].id_guardia,
+                    id_profesor: elements[key].id_profesor,
+                    ausente: elements[key].ausente,
+                    fecha: elements[key].fecha,
+                    hora: elements[key].hora
+                };
+                //arrayGuardias.set(guardia.id_profesor,nguardiashechas);
+
+                if(guardia.id_profesor == idProfesor){
+                    
+                    if(guardia.ausente == true){
                         outerHTML += "<td> SI </td>";
                     }else{
                         outerHTML += "<td> NO </td>";
                     }
-                    outerHTML += "<td> "+ elements[key].fecha +"</td>";
-                    outerHTML += "<td> "+ elements[key].hora +"</td>";
+                    outerHTML += "<td> "+ guardia.fecha +"</td>";
+                    outerHTML += "<td> "+ guardia.hora +"</td>";
                     outerHTML += "</tr>";  
                 }
-                                     
+                                    
             }
-            outerHTML += "</table>";
             
+            outerHTML += "</table>";
             elements = [];
             document.getElementById("guardias").innerHTML = outerHTML;
         };
@@ -223,13 +283,22 @@ function generateProfesorTable(){
                     
         var outerHTML = "";
         outerHTML += "<table>";
-        outerHTML += "<th>ID</th><th>Profesor</th><th>Guardias</th>";
+        outerHTML += "<th>ID</th><th>Profesor</th><th>Visualizar</th><th>Realizadas</th>";
         outerHTML += "<tr>";
         for (var key in elements) {
-            outerHTML += "<td> "+ elements[key].id_profesor +"</td>";
-            outerHTML += "<td> "+ elements[key].nombre +"</td>";
-            if(elements[key].guardias == undefined && elements[key].nombre != undefined){
-                outerHTML += "<td> <button onclick='generateGuardiaTable(this.id);' id='"+elements[key].id_profesor+"'>Visualizar</button></td>";
+
+            let profesor = {
+                id_profesor: elements[key].id_profesor,
+                nombre: elements[key].nombre,
+                guardias: undefined
+            }
+            arrayProfesores.set(Number(profesor.id_profesor),profesor);
+            nguardias.set(profesor.id_profesor,0);
+
+            outerHTML += "<td> "+ profesor.id_profesor +"</td>";
+            outerHTML += "<td> "+ profesor.nombre +"</td>";
+            if(profesor.nombre != undefined && profesor.nombre != ""){
+                outerHTML += "<td> <button onclick='generateGuardiaTable(this.id);' id='"+profesor.id_profesor+"'>Visualizar</button></td>";
             }
             outerHTML += "</tr>";                       
         }
@@ -237,6 +306,7 @@ function generateProfesorTable(){
         
         elements = [];
         document.getElementById("profesor").innerHTML = outerHTML;
+        console.log(arrayProfesores);
     };
 };
 }
