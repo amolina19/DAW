3fea6e76e7c402897d477049cedfcf5fe20bcc35
amolina19 <alejandro.molina.dam@gmail.com>
2021-03-01 12:47:46 +0100

    Fixed.

--- Entornos Servidor/Tema 4/Aplicacion Web/informacion.php
+++ Entornos Servidor/Tema 4/Aplicacion Web/informacion.php
@@ -35,20 +35,20 @@
                 Es una aplicación web sencilla que permita a un usuario acceder a una parte de la aplicación u otra en función de si se ha autentificado o no. Dicha aplicación constará de las siguientes páginas:
             </p>
             <ul>
-                <li><b>index.php</b>
+                <li><b>Index.php</b>
                     <p>Deberá ofrecer un formulario para que el usuario pueda introducir su usuario y contraseña y poder autentificarse. También debe ofrecer la posibilidad de acceder comoinvitado. Si se accede como invitado sólo podrá acceder a la página de información(información.php). En caso de que se introduzca un usuario y contraseña se comprobará que dicho usuario esté dado de alta en la base de datos (en la tabla usuarios). Si el usuario ycontraseña son correctos se creará una sesión y se tendrá acceso a la página aplicación.php. Las contraseñas están almacenadas en la base de datos usando hashing de una solavía mediante la función password_hash, por lo tanto para comprobar si una contraseña es correcta se deberá usar la función password_verify.</p>
                 </li>
-                <li><b>informacion.php</b>
+                <li><b>Informacion.php</b>
                     <p>Página que tan solo muestra la información de uso de la aplicación. En ella se debe explicar el funcionamiento de la aplicación. Debe ofrecer un enlace para volvera la página de inicio (index.php).</p>
                 </li>
-                <li><b>aplicacion.php</b>
+                <li><b>Aplicacion.php</b>
                     <p>A esta página sólo tendrán acceso los usuarios que hayan sido autentificados. Se controlará su acceso mediante sesiones (las sesiones almacenarán elnombre del usuario y la hora de conexión). Esta página debe ofrecer un menú que permit aacceder a la página de información (información.php), a la página de preferencias(preferencias.php) y a la desconexión del usuario.</p>
                 </li>
-                <li><b>preferencias.php</b>
+                <li><b>Preferencias.php</b>
                     <p>Esta página permitirá al usuario seleccionar el color de fondo con el que se mostrarán todas las páginas. Estas preferencias serán guardadas en una cookie. En caso deque no se hayan establecido preferencias el color por defecto será el blanco. Esta página también ofrecerá la opción de restablecer las preferencias (debe eliminar la cookie). </p>
                 </li>
-                <li><b>funciones.inc</b>
-                    <p>página que constará de las funciones usadas en la aplicación. Al menos constará de las funciones de acceso y control a la base de datos. </p>
+                <li><b>Funciones.inc</b>
+                    <p>Página que constará de las funciones usadas en la aplicación. Al menos constará de las funciones de acceso y control a la base de datos. </p>
                 </li>
             </ul>
 
