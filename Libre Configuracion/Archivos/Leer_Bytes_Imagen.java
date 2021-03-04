592abb397bad168d823bca07950bbc4dee908a8f
amolina19 <alejandro.molina.dam@gmail.com>
2021-03-04 09:27:54 +0100

    Tarea 3 Terminada.

---
+++ Libre Configuracion/Archivos/Leer_Bytes_Imagen.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Leer_Bytes_Imagen {
+
+    static String PATH = "Libre Configuracion/Archivos/";
+    static FileInputStream fis;
+    static FileOutputStream fos;
+    static File file;
+    static int bytes[];
+
+    public static void main(String[] args) {
+
+        try {
+            file = new File(PATH + "imagen.png");
+            fis = new FileInputStream(file);
+            leerImagen(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void leerImagen(FileInputStream fis) {
+        
+        try {
+            fos = new FileOutputStream(System.getProperty("user.home")+"/Escritorio/manosunidas.png");
+            int i;
+            bytes = new int[(int) file.length()];
+            int count = 0;
+            while((i = fis.read()) != -1){
+                bytes[count] = i;
+                count++;
+                //fos.write(i);
+            }
+
+            for(int x = 0; x < bytes.length;x++){
+                fos.write(bytes[x]);
+            }
+            fis.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
