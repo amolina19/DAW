0df6908c126cddaaa2ec5573085c43afbd0bbf8e
amolina19 <alejandro.molina.dam@gmail.com>
2021-03-08 14:41:17 +0100

    Dev admin panel

--- Entornos Servidor/Tema 4/AutosLocos/src/pages/modules.php
+++ Entornos Servidor/Tema 4/AutosLocos/src/pages/modules.php
@@ -19,14 +19,22 @@
                 }
             }
         }else{
-            echo    "<button class='btn btn-success mr-2' name='login'>Iniciar Sesion</button>";
+            echo   "<button class='btn btn-success mr-2' name='login'>Iniciar Sesion</button>";
             echo   "<button class='btn btn-primary mr-2' name='registrarse'>Registrarse</button>";
         }
        
         
         
         echo   "<input class='form-control mr-sm-2 mt-2 mt-sm-0' type='text' placeholder='Buscar vehiculo' name='buscar'>";
-        echo   "<button class='btn btn-outline-success my-2 my-sm-0' type='submit' name='buscarbtn'>Buscar</button>";
+        echo   "<button class='btn btn-outline-success my-2 my-sm-0 mr-4' type='submit' name='buscarbtn'>Buscar</button>";
+
+        if(isset($_SESSION['username']) && isset($_SESSION['password'])){
+            echo   "<div class='row'>";
+            echo   "<input class='ml-2 mr-2' type='image' src='../images/perfil.svg' height='40px' name='perfil'>";
+            echo   "<b>".$_SESSION['username']."</b>";
+            echo   "</div>";
+        }
+        
         echo  "</form>";
         echo  "</div>";
         echo  "</nav>";
@@ -44,8 +52,11 @@
         echo   "<form class='form-inline my-2 my-lg-0' method='post'>";
         if(isset($_SESSION['username']) && isset($_SESSION['password'])){
             if($_SESSION['username'] !== null && $_SESSION['password'] !== null && $_SESSION['type'] === 'admin'){
-                echo    "<button class='btn btn-danger mr-2' name='desconectar'>Desconectar</button>";
+                echo    "<input class='mr-2' type='image' src='../images/usuarios.svg' height='40px'name='moduser'>";
+                echo    "<input class='mr-2' type='image' src='../images/vehiculos.svg' height='40px'name='modvehiculo'>";
                 echo    "<button class='btn btn-warning mr-2' name='volver'>Volver</button>";
+                echo    "<button class='btn btn-danger mr-2' name='desconectar'>Desconectar</button>";
+                
             }
         }
         echo  "</form>";
